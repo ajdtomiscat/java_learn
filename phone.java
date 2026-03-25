f8913b4f91b431966a5224842dc933e75c1b57a3
degas99 <2936965582@qq.com>
2026-03-25 23:12:44 +0800

    Add files via upload

---
+++ phone.java
@@ -0,0 +1,11 @@
+package newpro;
+public class phone {
+    String name;
+    int price;
+    void paly(){
+        System.out.println("玩手机");
+    }
+    void photo(){
+        System.out.println("拍照");
+    }
+}
---
+++ things.java
@@ -0,0 +1,21 @@
+package newpro;
+
+public class things {
+   
+
+public void main(String[] args){
+
+    phone xm10=new phone();
+
+    xm10.name="小米10";
+    xm10.price=4399;
+    System.out.println("手机品牌："+xm10.name+" 手机价格："+xm10.price);
+    xm10.paly();
+    phone iPhone=new phone();
+    iPhone.name="iphone17";
+    iPhone.price=5499;
+    System.out.println(iPhone.name);
+}
+}
+
+
